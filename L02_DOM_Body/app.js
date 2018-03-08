f8fe6c1623f5123730cc3be1f11ca8b5fca287a4
hyf_syp <hyf@syp.com>
2018-03-08 14:35:41 +0800

    body节点

---
+++ L02_DOM_Body/app.js
@@ -0,0 +1,10 @@
+(function () {
+
+    // for (var i = 0; i < 100; i++) {
+    //     // document.write("Hello World");
+    //     // document.writeln("Hello World");
+    //     document.write("Hello World<br>");
+    // }
+
+    document.body.innerHTML = "Hello <br> World";
+})();
